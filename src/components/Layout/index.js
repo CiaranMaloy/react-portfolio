c06ac1a1794bf76957b489ad577055e399226239
Ciaran Maloy <1ciaran.maloy@gmail.com>
2024-01-10 13:23:57 +0000

    added homepage with some stylings

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,29 @@
 import './index.scss'
 import Sidebar from '../Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-    return <><Sidebar/></>
+    return (
+    <div className="App">
+        <Sidebar/>
+        <div className='page'>
+            <span className='tags top-tags'>
+                &ls;body&gt;
+            </span>
+
+            <Outlet />
+
+            <span className='tags bottom-tags'>
+                &ls;/body&gt;
+                <br />
+                <span className='bottom-tag-html'>
+                    &ls;/body&gt;
+                </span>
+            </span>
+
+        </div>
+    </div>
+    )
 }
 
 export default Layout
