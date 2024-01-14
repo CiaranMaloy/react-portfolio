262c5710e502867619a4e1676b33f48060f8aaaf
Ciaran Maloy <1ciaran.maloy@gmail.com>
2024-01-14 19:09:47 +0000

    tutorial complete

---
+++ src/components/Contact/index.js
@@ -0,0 +1,107 @@
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const Contact = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'default_service',
+                'template_h3zf3ld',
+                refForm.current,
+                '3EEdxQJz_dWPH7IEq'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Message failed to send, please try again!')
+                }
+            )
+    }
+
+    const position = [51.505, -0.09]
+
+    return (
+        <>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                            idx={15} />
+                    </h1>
+                    <p>
+                        don't
+                    </p>
+                    <div className='contact-form'>
+                        <form ref={refForm} onSubmit={sendEmail}>
+                            <ul>
+                                <li className='half'>
+                                    <input type='text' name='name' placeholder='Name' required />
+                                </li>
+                                <li className='half'>
+                                    <input type='email' name='email' placeholder='Email' required />
+                                </li>
+                                <li>
+                                    <input placeholder="Subject" type="text" name="subject" required />
+                                </li>
+                                <li>
+                                    <textarea placeholder="Message" name="message" required></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className='flat-button' value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+                <div className='info-map'>
+                    Ciaran Maloy,
+                    <br />
+                    United Kingdom
+                    <br />
+                    London
+                    <br />
+                    SE1 4DU
+                    <br />
+                    <span>1ciaran.maloy@gmail.com</span>
+                </div>
+                <div className="map-wrap">
+                    <MapContainer center={position} zoom={15} scrollWheelZoom={true}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={position}>
+                            <Popup>
+                                Ciaran lives here. <br /> Come over for a cup of tea.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Contact
